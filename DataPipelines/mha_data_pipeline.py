ca8c1dcd8207c5c49b0f1823ac4a46b81e4f63db
Ikochoy <kwan.choy@mail.utoronto.ca>
2021-09-10 23:30:18 -0400

    initial push

---
+++ DataPipelines/mha_data_pipeline.py
@@ -0,0 +1,15 @@
+# class mha_data_pipeline:
+# def step_1(dataframes: List[pd.df]):
+
+# def step_2 
+
+
+# def __call__(self):
+#     step_1()
+#     step_2()
+#     ...
+
+
+# ###: https://www.geeksforgeeks.org/__call__-in-python/
+# datapipeline = mha_data_pipeline()
+# datapipeline()
