9049d0c23de18f5e1e5c0e83cb3d6d4d75c9d64a
Ikochoy <kwan.choy@mail.utoronto.ca>
2021-09-13 15:35:36 -0400

    add exceptions docstrings

--- Exceptions/DataDownloaderException.py
+++ Exceptions/DataDownloaderException.py
@@ -1,6 +1,10 @@
 class ConfigsNotSetException(Exception):
+    """Exception raised when the configs are not loaded properly
+    """
     pass
 
 
 class RequestFailureException(Exception):
+    """Exception raised when the API call fail
+    """
     pass
